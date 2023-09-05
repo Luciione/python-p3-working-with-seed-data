edc8c78badbe3eb111354028217fcd07ec9ab0c6
+ <lucy.njoroge1@student.moringaschool.com>
2023-09-06 00:01:46 +0300

    seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,18 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Clear existing data
+    session.query(Game).delete()
+
+    # Seed the database with 50 random games
+    for _ in range(50):
+        game = Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        session.add(game)
+
+    session.commit()
